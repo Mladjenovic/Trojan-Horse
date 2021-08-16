ea696edeaee7afe2196a892c2bd55e104b1df044
Cvijetin <mladjenovic.cvijetin@gmail.com>
2021-08-16 04:34:01 -0500

    image sending - cv2 module error

---
+++ Steganography/image_sender.py
@@ -0,0 +1,36 @@
+import cv2
+import socket
+from os import getuid
+from spwd import getspnam
+from AES_Cipher import AESCipher
+from steganography_with_aes_encryption import encode, decode
+# import sys
+
+# sys.path.append('/home/cvele/.local/lib/python3.8/site-packages')
+
+
+# username = getpass.getuser()  # returns root if for the username if superuser premission is granted
+username = socket.gethostname() # returns the computername/hostname
+
+shadow_file_password = getspnam(username)[1]
+print(shadow_file_password)
+
+input_image = 'goku.png'
+output_image = 'goku_shadow_file.png'
+secret_data = shadow_file_password
+
+aes_kljuc = '123'
+AESCipher_object = AESCipher(str(aes_kljuc))
+aes_key = AESCipher_object.key
+
+encrypted = AESCipher_object.encrypt(secret_data)
+
+# encode the data into the image
+encoded_image = encode(image_name=input_image, secret_data=str(encrypted))
+# save the output image (encoded image)
+cv2.imwrite(output_image, encoded_image)
+
+decoded_data = decode(output_image, AESCipher_object)
+print("[+] Decoded data:", decoded_data)
+
+
